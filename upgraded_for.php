872cb46a8ebc723daf9318b7223ec465d03be72b
Guillermo Latumahina <glatumahina@roc-dev.com>
2019-09-19 11:39:29 +0200

    Uitwerking van de exercise

---
+++ upgraded_for.php
@@ -0,0 +1,22 @@
+<?php
+$leerlingen = array(
+    "Guillermo Latumahina",
+    "Daniel Kempers",
+    "Daniel van den Brink",
+    "Sven van 't Hoenderdaal",
+    "Maurits Does",
+    "Quincy van Kempen",
+    "Sonny Proeger",
+    "Thorval van Beek",
+    "Tom Paulus",
+    "Joshua Barselaar",
+    "Amjad Alarori",
+    "Maro",
+    "Marwan Moussaten",
+    "Mitchel Meskes",
+    "Silvano Bakacak"
+);
+
+foreach($leerlingen as $i){
+    echo $i .PHP_EOL;
+}
